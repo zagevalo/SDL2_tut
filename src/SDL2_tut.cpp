2e864a24edf96c253c416fd2c38bbe6f62a61139
zagevalo <zagevalo@gmail.com>
2014-03-05 18:04:42 +0400

    Updated spritsheet and fixed simple moving.

--- src/SDL2_tut.cpp
+++ src/SDL2_tut.cpp
@@ -9,6 +9,7 @@
 const int SCREEN_WIDTH = 1024;
 const int SCREEN_HEIGHT = 768;
 const int TILE_SIZE = 40;
+const int SPRITE_SIZE = 36;
 
 
 int main(int argc, char **argv) {
@@ -47,41 +48,38 @@ int main(int argc, char **argv) {
 
 
 	SDL_Texture *bg = loadTexture("res/bg.png", renderer);
-	SDL_Texture *character = loadTexture("res/Alice.png", renderer);
+	SDL_Texture *character = loadTexture("res/Alice_spritesheet.png", renderer);
 	if (bg == nullptr || character == nullptr)
 		return 4;
 
 
 	// Main Loop
 
+	SDL_Rect char_clips[8][4];
+	for (int i=0; i < 8; i++) {
+		for (int j=0; j < 4; j++) {
+			char_clips[i][j].x = j * SPRITE_SIZE;
+			char_clips[i][j].y = i * SPRITE_SIZE;
+			char_clips[i][j].w = SPRITE_SIZE;
+			char_clips[i][j].h = SPRITE_SIZE;
+		}
+	}
+
+	int direction_state = 4; // 0 - up, 1 - up_right, 2 - right, 3 - right_down, 4 - down,
+							 // 5 - down_left, 6 - left, 7 -left_up
+
+	int anim_frame = 0;
+
 	SDL_Rect char_box;
-	int iW, iH;
-	SDL_QueryTexture(character, NULL, NULL, &iW, &iH);
-	char_box.x = SCREEN_WIDTH / 2 - iW / 2;
-	char_box.y = SCREEN_HEIGHT / 2 - iH / 2;
-	char_box.w = iW + 20;
-	char_box.h = iH + 20;
+	char_box.x = SCREEN_WIDTH / 2 - SPRITE_SIZE * 2;
+	char_box.y = SCREEN_HEIGHT / 2 - SPRITE_SIZE * 2;
+	char_box.w = SPRITE_SIZE * 4;
+	char_box.h = SPRITE_SIZE * 4;
 
 	SDL_Event e;
 	bool quit = false;
 
 	while (!quit) {
-		while (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT)
-				quit = true;
-			if (e.type == SDL_KEYDOWN)
-				switch(e.key.keysym.sym) {
-					case SDLK_LEFT: char_box.x -= 20; break;
-					case SDLK_RIGHT: char_box.x += 20; break;
-					case SDLK_UP: char_box.y -= 20; break;
-					case SDLK_DOWN: char_box.y += 20; break;
-					case SDLK_ESCAPE: quit = true; break;
-				}
-			if (e.type == SDL_MOUSEBUTTONDOWN)
-				quit = true;
-		}
-
-		// Render images
 
 		SDL_RenderClear(renderer);
 
@@ -95,7 +93,56 @@ int main(int argc, char **argv) {
 		bg_box.h = SCREEN_HEIGHT;
 		renderTexture(bg, renderer, bg_box, nullptr);
 
-		renderTexture(character, renderer, char_box.x, char_box.y, nullptr);
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT)
+				quit = true;
+			if (e.type == SDL_KEYDOWN)
+				switch(e.key.keysym.sym) {
+					case SDLK_LEFT:
+						if (char_box.x > 0)
+							char_box.x -= 20;
+						direction_state = 6;
+						if (anim_frame >= 3)
+							anim_frame = 0;
+						else
+							++anim_frame;
+						break;
+					case SDLK_RIGHT:
+						if (char_box.x < (SCREEN_WIDTH - SPRITE_SIZE * 4))
+							char_box.x += 20;
+						direction_state = 2;
+						if (anim_frame >= 3)
+							anim_frame = 0;
+						else
+							++anim_frame;
+						break;
+					case SDLK_UP:
+						if (char_box.y > 0)
+								char_box.y -= 20;
+						direction_state = 0;
+						if (anim_frame >= 3)
+							anim_frame = 0;
+						else
+							++anim_frame;
+						break;
+					case SDLK_DOWN:
+						if (char_box.y < (SCREEN_HEIGHT - SPRITE_SIZE * 4))
+							char_box.y += 20;
+						direction_state = 4;
+						if (anim_frame >= 3)
+							anim_frame = 0;
+						else
+							++anim_frame;
+						break;
+					case SDLK_ESCAPE:
+						quit = true;
+						break;
+				}
+			if (e.type == SDL_MOUSEBUTTONDOWN)
+				quit = true;
+		}
+
+		renderTexture(character, renderer, char_box, &char_clips[direction_state][anim_frame]);
 
 		SDL_RenderPresent(renderer);
 	}
